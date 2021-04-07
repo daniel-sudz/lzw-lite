8b63f275faaddd25849fe5c2ab4ef60c9b9a88df
Daniel Sudzilouski <daniel@sudzilouski.com>
2021-04-06 22:10:15 -0700

    decompress

--- src/trie.ts
+++ src/trie.ts
@@ -1,5 +1,6 @@
 // http://warp.povusers.org/EfficientLZW/part3.html
 // Optimizing for dynamic bit sizes
+// note: applicable to sending data to server
 
 interface node {
   /** Emit code of sequence */
@@ -36,6 +37,7 @@ const buildDictionary = (reverse: boolean = false) => {
 
 export const compress = (inputString: string) => {
   const uint8Array = new TextEncoder().encode(inputString);
+  console.log(uint8Array);
   const dictionary = buildDictionary() as node;
   let currentCode = 256;
   const emit: number[] = [];
@@ -52,17 +54,46 @@ export const compress = (inputString: string) => {
     // push sequence + next byte to dictionary
     if (i + 1 < uint8Array.length) {
       currentByte!.children[uint8Array[i + 1]] = {
-        code: currentCode + 1,
+        code: currentCode,
         children: {},
       };
       currentCode++;
     }
   }
-  console.log(emit);
+  //console.log(emit);
+  return emit;
 };
+
+interface reverseDictionary {
+  /** Sequence code -> byte sequence */
+  [key: number]: number[];
+}
 
 export const decompress = (codeArray: number[]) => {
-  const dictionary = buildDictionary() as node;
+  const emit: number[] = [];
+  let currentCode = 256;
+  const dictionary: reverseDictionary = {};
+  // Build intial dictionary
+  for (let i = 0; i < 256; i++) {
+    dictionary[i] = [i];
+  }
+  // iterate through code array
+  for (let i = 0; i < codeArray.length; i++) {
+    const curSequence = dictionary[codeArray[i]];
+    const nextSequence = dictionary[codeArray[i + 1]];
+    // next sequence cannot be decoded, first char must equal first char in current sequence
+    if (nextSequence === undefined) {
+      dictionary[currentCode] = [...curSequence, curSequence[0]];
+    }
+    // next sequence can be decoded
+    else {
+      dictionary[currentCode] = [...curSequence, nextSequence[0]];
+    }
+    emit.push(...curSequence);
+    currentCode++;
+  }
+  console.log(emit);
+  return emit;
 };
-
-compress("😆😆😆😆😆😆");
+//compress("😆😆");
+decompress(compress("😆😆😆😆"));
